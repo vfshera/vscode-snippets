9cb6eb45f125dd6694275891418c529dcabcc532
Franklin Shera <fshera96@gmail.com>
2025-05-27 02:38:21 +0300

    add cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,77 @@
 #!/usr/bin/env node
 
-import path from "node:path";
-const SNIPPETS_DIR = path.join(import.meta.dirname, "../snippets");
+import * as p from "@clack/prompts";
+import color from "picocolors";
+import process from "node:process";
+import commands from "./commands.js";
+import { getPackageName, getVersion, sleep } from "./utils.js";
 
-console.log(
-  `Welcome to the CLI! The snippets directory is located at: ${SNIPPETS_DIR}`
-);
+async function showHelp() {
+  p.log.message(color.bold("Available commands:"));
+  for (const command of Object.values(commands)) {
+    p.log.info(
+      `${color.cyanBright(
+        color.bold(command.name) +
+          (command.alias
+            ? ` or ${color.italic(command.alias.join(" | "))}`
+            : "")
+      )} - ${command.description}${
+        command.usage
+          ? color.italic(`\nUsage: npx ${getPackageName()} ${command.usage}`)
+          : ""
+      }`
+    );
+  }
+  console.log();
+}
+
+function getCommand(commandKey) {
+  const command =
+    commands[commandKey] ||
+    Object.values(commands).find(
+      (cmd) => cmd.alias && cmd.alias.includes(commandKey)
+    );
+
+  if (command && command.name === "help") {
+    command.action = async () => {
+      await showHelp();
+    };
+  }
+  return command;
+}
+
+async function main() {
+  console.clear();
+
+  await sleep(1000);
+
+  p.intro(
+    [
+      color.bgCyan(color.black(` ${getPackageName()} `)),
+      color.bgWhite(color.black(` v${getVersion()} `)),
+    ].join("")
+  );
+
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    await showHelp();
+    return;
+  }
+
+  const [key, ...rest] = args;
+
+  const command = getCommand(key);
+
+  if (!command) {
+    console.log();
+
+    p.log.error(`Command "${key}" not found.`);
+
+    return;
+  }
+
+  await command.action(rest);
+}
+
+main().catch(console.log);
